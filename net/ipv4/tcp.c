eeedb2ac5f75369bfdb94636ce5f65d6fcbd0f33
Samuel Pascua <pascua.samuel.14@gmail.com>
2023-03-29 08:39:52 +0800

    net: tcp: add back mptcp-related check
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -3310,7 +3310,14 @@ static int do_tcp_setsockopt(struct sock *sk, int level,
 #ifdef CONFIG_TCP_MD5SIG
 	case TCP_MD5SIG:
 	case TCP_MD5SIG_EXT:
+#ifdef CONFIG_MPTCP
+	if ((1 << sk->sk_state) & (TCPF_CLOSE | TCPF_LISTEN) && !sock_flag(sk, SOCK_MPTCP))
 		err = tp->af_specific->md5_parse(sk, optname, optval, optlen);
+	else
+		err = -EINVAL;
+#else
+		err = tp->af_specific->md5_parse(sk, optname, optval, optlen);
+#endif
 		break;
 #endif
 	case TCP_USER_TIMEOUT:
