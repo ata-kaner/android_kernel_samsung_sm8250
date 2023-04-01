eff5b17338c741ed2a5cabad426a7ab92297160d
Samuel Pascua <pascua.samuel.14@gmail.com>
2023-04-01 14:36:00 +0800

    devfreq: fix return value when preventing min_freq writes
    sysfs entries should either return the number of bytes written/read or a negative value if an error occurs
    
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -1218,7 +1218,7 @@ static ssize_t min_freq_store(struct device *dev, struct device_attribute *attr,
 	int ret;
 
 	if (likely(task_is_booster(current)))
-		return 0;
+		return -EINVAL;
 
 	ret = sscanf(buf, "%lu", &value);
 	if (ret != 1)
