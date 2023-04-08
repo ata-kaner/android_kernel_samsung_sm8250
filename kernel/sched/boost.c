01c5bca375548b7cf8b399a0b8f909ed04945dd5
Samuel Pascua <pascua.samuel.14@gmail.com>
2023-04-08 11:32:13 +0800

    sched: boost: disallow userspace sched_boost()
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- kernel/sched/boost.c
+++ kernel/sched/boost.c
@@ -6,6 +6,7 @@
 #include "sched.h"
 #include "walt.h"
 #include <linux/of.h>
+#include <linux/binfmts.h>
 #include <linux/sched/core_ctl.h>
 #include <trace/events/sched.h>
 
@@ -269,6 +270,9 @@ int sched_boost_handler(struct ctl_table *table, int write,
 {
 	int ret;
 	unsigned int *data = (unsigned int *)table->data;
+
+	if (task_is_booster(current))
+		return 0;
 
 	mutex_lock(&boost_mutex);
 
