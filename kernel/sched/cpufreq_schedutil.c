e835b63487eb8b21286c2c91a7e815c8ca680679
Samuel Pascua <pascua.samuel.14@gmail.com>
2023-12-28 17:38:52 +0800

    schedutil: avoid userspace writing down_rate_limit_us
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -600,7 +600,7 @@ static ssize_t down_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
 
-	if (task_is_booster(current))
+	if (task_is_booster(current) || current->parent->pid == 1)
 		return count;
 
 	if (kstrtouint(buf, 10, &rate_limit_us))
