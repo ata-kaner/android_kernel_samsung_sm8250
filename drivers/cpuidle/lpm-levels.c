1ad8c67e1a75a073c893e128cc110ef3e1ce0157
Samuel Pascua <pascua.samuel.14@gmail.com>
2024-04-06 21:27:14 +0800

    cpuidle: fix build without debugging
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -1692,9 +1692,6 @@ static int lpm_suspend_prepare(void)
 #ifdef CONFIG_SEC_PM
 	regulator_showall_enabled();
 	clock_debug_print_enabled();
-
-	debug_masterstats_show("entry");
-	debug_rpmstats_show("entry");
 #endif /* CONFIG_SEC_PM */
 
 #ifdef CONFIG_SEC_PM_DEBUG
@@ -1713,11 +1710,6 @@ static void lpm_suspend_wake(void)
 {
 	suspend_in_progress = false;
 	lpm_stats_suspend_exit();
-
-#ifdef CONFIG_SEC_PM
-	debug_rpmstats_show("exit");
-	debug_masterstats_show("exit");
-#endif
 }
 
 static int lpm_suspend_enter(suspend_state_t state)
