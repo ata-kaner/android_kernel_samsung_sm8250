c0e47635cdc34cf5f5469c845165acbdaad4af83
Atakan <ata.kaner03@gmail.com>
2025-03-22 10:51:16 +0300

    Revert "bpf: cpumap use ptr_ring_consume_batched"
    This reverts commit 63ee3d8c2a7b92618c98e98d2d2fc391d7598e23.

--- kernel/bpf/cpumap.c
+++ kernel/bpf/cpumap.c
@@ -240,8 +240,6 @@ static void put_cpu_map_entry(struct bpf_cpu_map_entry *rcpu)
 	}
 }
 
-#define CPUMAP_BATCH 8
-
 static int cpu_map_kthread_run(void *data)
 {
 	struct bpf_cpu_map_entry *rcpu = data;
@@ -254,9 +252,8 @@ static int cpu_map_kthread_run(void *data)
 	 * kthread_stop signal until queue is empty.
 	 */
 	while (!kthread_should_stop() || !__ptr_ring_empty(rcpu->queue)) {
-		unsigned int drops = 0, sched = 0;
-		void *frames[CPUMAP_BATCH];
-		int i, n;
+		unsigned int processed = 0, drops = 0, sched = 0;
+		struct xdp_frame *xdpf;
 
 		/* Release CPU reschedule checks */
 		if (__ptr_ring_empty(rcpu->queue)) {
@@ -272,16 +269,14 @@ static int cpu_map_kthread_run(void *data)
 			sched = cond_resched();
 		}
 
+		/* Process packets in rcpu->queue */
+		local_bh_disable();
 		/*
 		 * The bpf_cpu_map_entry is single consumer, with this
 		 * kthread CPU pinned. Lockless access to ptr_ring
 		 * consume side valid as no-resize allowed of queue.
 		 */
-		n = ptr_ring_consume_batched(rcpu->queue, frames, CPUMAP_BATCH);
-
-		local_bh_disable();
-		for (i = 0; i < n; i++) {
-			struct xdp_frame *xdpf = frames[i];
+		while ((xdpf = __ptr_ring_consume(rcpu->queue))) {
 			struct sk_buff *skb;
 			int ret;
 
@@ -295,9 +290,13 @@ static int cpu_map_kthread_run(void *data)
 			ret = netif_receive_skb_core(skb);
 			if (ret == NET_RX_DROP)
 				drops++;
+
+			/* Limit BH-disable period */
+			if (++processed == 8)
+				break;
 		}
 		/* Feedback loop via tracepoint */
-		trace_xdp_cpumap_kthread(rcpu->map_id, n, drops, sched);
+		trace_xdp_cpumap_kthread(rcpu->map_id, processed, drops, sched);
 
 		local_bh_enable(); /* resched point, may call do_softirq() */
 	}
