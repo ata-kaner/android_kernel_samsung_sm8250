9241626ae95400f72a151ced96c6da99a9a1af36
Atakan <ata.kaner03@gmail.com>
2025-04-01 12:03:31 +0300

    Revert "techpack: display: msm: samsung: Define fod-dim-lut table"
    This reverts commit ea24e9d0665c3d3c8d84ebf95a4459d4dafbf00d.

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -3170,7 +3170,7 @@ error:
 static int dsi_panel_parse_fod_dim_lut(struct dsi_panel *panel,
 		struct dsi_parser_utils *utils)
 {
-	const char *prop_name = "samsung,fod-dim-lut";
+	const char *prop_name = "qcom,fod-dim-lut";
 	unsigned int i;
 	u32 *array;
 	int count;
